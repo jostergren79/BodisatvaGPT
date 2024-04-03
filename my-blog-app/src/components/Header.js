fa6508c4ecff11a150931c79eaa0e3eb49b92b88
Jason Ostergren <jason@Jasons-MacBook-Pro-3.local>
2024-04-03 16:53:22 -0500

    working on another app

--- my-blog-app/src/components/Header.js
+++ my-blog-app/src/components/Header.js
@@ -1,17 +1,10 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import MenuIcon from '@mui/icons-material/Menu';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
 import logo from "../images/logoedit1-transformed.webp"
 import "./Header.css"
 
 const Header = () => {
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!isMobileMenuOpen);
-    };
+  
 
     return (
         <header className='header'>
@@ -23,14 +16,12 @@ const Header = () => {
                   </div>
             
             <nav className="navigation nav-links">
-                <ul className={isMobileMenuOpen ? 'mobile-menu open' && 'desktop-menu' : 'navList'}>
+                <ul >
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/blog">Blog</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
-                <IconButton className="mobile-menu-icon" onClick={toggleMobileMenu}>
-                    {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
-                </IconButton>
+        
             </nav>
         </header>
     );
