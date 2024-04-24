7eba692e3d4377967110acfa72d50b9567b897b9
Jason Ostergren <jason@Jasons-MacBook-Pro-3.local>
2024-04-23 22:28:49 -0500

    initial work on portfolio transformed into creation of memory game

---
+++ portfoliopage/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroCard from './components/HeroCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroCard />
+    </div>
+  );
+}
+
+export default App;
