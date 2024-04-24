ab743064af493d0b6757da403cab12298584125d
Jason Ostergren <jason@Jasons-MacBook-Pro-3.local>
2024-04-24 15:12:39 -0500

    putting together the styling id like to use doing some experimenting

--- portfoliopage/src/components/HeroCard/HeroCard.js
+++ portfoliopage/src/components/HeroCard/HeroCard.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
-import './HeroCard.css';  // Make sure the CSS path matches
-import image1 from '../../assets/images/image1.png'
+import React, { useState, useEffect } from 'react';
+import './HeroCard.css';
+import bunny from '../../assets/images/bunny.png';
+import cat from '../../assets/images/cat.png';
+import dog from '../../assets/images/dog.png';
+import fish1 from '../../assets/images/fish1.png';
+import fish2 from '../../assets/images/fish2.png';
+import hamster from '../../assets/images/hamster.png';
+import kitten from '../../assets/images/kitten.png';
+import lizard from '../../assets/images/lizard.png';
+import parrot from '../../assets/images/parrot.png';
+import turtle1 from '../../assets/images/turtle1.png';
+import turtle2 from '../../assets/images/turtle2.png';
+
+const imageList = [bunny, cat, dog, fish1, fish2, hamster, kitten, lizard, parrot, turtle1, turtle2];
 
 function HeroCard() {
-    // Creating an array of 20 cards with unique ids, random numbers, and flipped states
-    const [cards, setCards] = useState(Array.from({ length: 20 }, (_, index) => ({
-        id: index,
-        number: Math.floor(Math.random() * 100),
-        flipped: false
-    })));
+    const [cards, setCards] = useState([]);
 
-    // Function to toggle the flipped state of a card
+    useEffect(() => {
+        let preparedCards = shuffleAndPrepareCards(imageList);
+        setCards(preparedCards);
+    }, []);
+
+    function shuffleAndPrepareCards(imageList) {
+        const doubledList = imageList.concat(imageList);
+        return doubledList
+            .map((src, index) => ({ id: index, src, flipped: false }))
+            .sort(() => Math.random() - 0.5);
+    }
+
     const toggleFlip = (id) => {
-        setCards(cards.map(card => 
-            card.id === id ? {...card, flipped: !card.flipped} : card
+        setCards(cards.map(card =>
+            card.id === id ? { ...card, flipped: !card.flipped } : card
         ));
     };
 
@@ -23,11 +41,10 @@ function HeroCard() {
                 <div className="card" key={card.id} onClick={() => toggleFlip(card.id)}>
                     <div className={`card-inner ${card.flipped ? 'is-flipped' : ''}`}>
                         <div className="card-front">
-                            {/* Image placed here to be the only visible content on the card */}
-                            <img src={ image1} alt="Pixelated Me" />
+                            <span>Memories</span> {/* Use a span for better control */}
                         </div>
                         <div className="card-back">
-                            More info
+                            <img src={card.src} alt="Animal" />
                         </div>
                     </div>
                 </div>
