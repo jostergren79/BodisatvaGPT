d6cbd25a10d596af8c1cf1b94abd26718918ad32
Jason Ostergren <jason@Jasons-MacBook-Pro-3.local>
2024-04-24 10:45:46 -0500

    updated styling for memory game

--- portfoliopage/src/App.js
+++ portfoliopage/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 import HeroCard from './components/HeroCard';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <HeroCard />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <HeroCard />
+        </div>
+    );
 }
 
 export default App;
---
+++ portfoliopage/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'; // Make sure this CSS path is correct
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>J & T's Memories</h1>
+    </div>
+  );
+}
+
+export default Header;
